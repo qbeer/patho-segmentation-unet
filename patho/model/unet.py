e6021b29171f685710a11b276f460ceff9247257
Alex Olar <olaralex666@gmail.com>
2019-06-27 19:00:21 +0200

    propbably solved the issue

--- patho/model/unet.py
+++ patho/model/unet.py
@@ -12,21 +12,29 @@ class UNET(Module):
         self.uplayer_1_conv1 = Conv2d(128, 64, 3)
         self.uplayer_1_conv2 = Conv2d(64, 64, 3)
         self.uplayer_1_conv3 = Conv2d(64, 1, 1)
+        self.upconv_1 = ConvTranspose2d(
+            128, 64, 2, padding=1, dilation=2, output_padding=0)
         # Layer 2
         self.layer_2_conv1 = Conv2d(64, 128, 3)
         self.layer_2_conv2 = Conv2d(128, 128, 3)
         self.uplayer_2_conv1 = Conv2d(256, 128, 3)
         self.uplayer_2_conv2 = Conv2d(128, 128, 3)
+        self.upconv_2 = ConvTranspose2d(
+            256, 128, 2, padding=1, dilation=2, output_padding=0)
         # Layer 3
         self.layer_3_conv1 = Conv2d(128, 256, 3)
         self.layer_3_conv2 = Conv2d(256, 256, 3)
         self.uplayer_3_conv1 = Conv2d(512, 256, 3)
         self.uplayer_3_conv2 = Conv2d(256, 256, 3)
+        self.upconv_3 = ConvTranspose2d(
+            512, 256, 2, padding=1, dilation=2, output_padding=0)
         # Layer 4
         self.layer_4_conv1 = Conv2d(256, 512, 3)
         self.layer_4_conv2 = Conv2d(512, 512, 3)
         self.uplayer_4_conv1 = Conv2d(1024, 512, 3)
         self.uplayer_4_conv2 = Conv2d(512, 512, 3)
+        self.upconv_4 = ConvTranspose2d(
+            1024, 512, 2, padding=1, dilation=2, output_padding=0)
         # Layer 5
         self.layer_5_conv1 = Conv2d(512, 1024, 3)
         self.layer_5_conv2 = Conv2d(1024, 1024, 3)
@@ -73,33 +81,37 @@ class UNET(Module):
         print("DONE!")
 
         # Up forward pass
-        up_layer_4 = interpolate(layer5, scale_factor=2, mode='bilinear', align_corners=True)
+        up_layer_4 = interpolate(
+            layer5, scale_factor=2, mode='bilinear', align_corners=True)
         print("DONE!")
-        up_layer_4 = ConvTranspose2d(1024, 512, 2, padding=1, dilation=2, output_padding=0)(up_layer_4)
+        up_layer_4 = self.upconv_4(up_layer_4)
         up_layer_4 = torch.cat((layer4_crop, up_layer_4), dim=1)
         up_layer_4 = self.uplayer_4_conv1(up_layer_4)
         up_layer_4 = relu(up_layer_4)
         up_layer_4 = self.uplayer_4_conv2(up_layer_4)
         up_layer_4 = relu(up_layer_4)
 
-        up_layer_3 = interpolate(up_layer_4, scale_factor=2, mode='bilinear', align_corners=True)
-        up_layer_3 = ConvTranspose2d(512, 256, 2, padding=1, dilation=2, output_padding=0)(up_layer_3)
+        up_layer_3 = interpolate(
+            up_layer_4, scale_factor=2, mode='bilinear', align_corners=True)
+        up_layer_3 = self.upconv_3(up_layer_3)
         up_layer_3 = torch.cat((layer3_crop, up_layer_3), dim=1)
         up_layer_3 = self.uplayer_3_conv1(up_layer_3)
         up_layer_3 = relu(up_layer_3)
         up_layer_3 = self.uplayer_3_conv2(up_layer_3)
         up_layer_3 = relu(up_layer_3)
 
-        up_layer_2 = interpolate(up_layer_3, scale_factor=2, mode='bilinear', align_corners=True)
-        up_layer_2 = ConvTranspose2d(256, 128, 2, padding=1, dilation=2, output_padding=0)(up_layer_2)
+        up_layer_2 = interpolate(
+            up_layer_3, scale_factor=2, mode='bilinear', align_corners=True)
+        up_layer_2 = self.upconv_2(up_layer_2)
         up_layer_2 = torch.cat((layer2_crop, up_layer_2), dim=1)
         up_layer_2 = self.uplayer_2_conv1(up_layer_2)
         up_layer_2 = relu(up_layer_2)
         up_layer_2 = self.uplayer_2_conv2(up_layer_2)
         up_layer_2 = relu(up_layer_2)
 
-        up_layer_1 = interpolate(up_layer_2, scale_factor=2, mode='bilinear', align_corners=True)
-        up_layer_1 = ConvTranspose2d(128, 64, 2, padding=1, dilation=2, output_padding=0)(up_layer_1)
+        up_layer_1 = interpolate(
+            up_layer_2, scale_factor=2, mode='bilinear', align_corners=True)
+        up_layer_1 = self.upconv_1(up_layer_1)
         up_layer_1 = torch.cat((layer1_crop, up_layer_1), dim=1)
         up_layer_1 = self.uplayer_1_conv1(up_layer_1)
         up_layer_1 = relu(up_layer_1)
