8000a4b492929acafe9fdf32cf6a33d4036d2dd2
Alex Olar <olaralex666@gmail.com>
2019-06-27 16:22:36 +0200

    debugging

--- patho/model/unet.py
+++ patho/model/unet.py
@@ -1,5 +1,5 @@
 from torch.nn import Module, Conv2d, ConvTranspose2d, MaxPool2d
-from torch.nn.functional import pad, relu, sigmoid, upsample_bilinear
+from torch.nn.functional import pad, relu, sigmoid, interpolate
 import torch
 
 
@@ -78,7 +78,7 @@ class UNET(Module):
         print(layer4.shape, layer4_crop.shape)
 
         # Up forward pass
-        up_layer_4 = upsample_bilinear(layer5, scale_factor=2)
+        up_layer_4 = interpolate(layer5, scale_factor=2, mode='bilinear')
         up_layer_4 = ConvTranspose2d(1024, 512, 2)(up_layer_4)
         print(up_layer_4.shape)
         up_layer_4 = torch.cat((layer4_crop, up_layer_4), dim=0)
@@ -87,7 +87,7 @@ class UNET(Module):
         up_layer_4 = self.uplayer_4_conv2(up_layer_4)
         up_layer_4 = relu(up_layer_4)
 
-        up_layer_3 = upsample_bilinear(up_layer_4, scale_factor=2)
+        up_layer_3 = interpolate(up_layer_4, scale_factor=2, mode='bilinear')
         up_layer_3 = ConvTranspose2d(512, 256, 2)(up_layer_3)
         up_layer_3 = torch.cat((layer3_crop, up_layer_3), dim=0)
         up_layer_3 = self.uplayer_3_conv1(up_layer_3)
@@ -95,7 +95,7 @@ class UNET(Module):
         up_layer_3 = self.uplayer_3_conv2(up_layer_3)
         up_layer_3 = relu(up_layer_3)
 
-        up_layer_2 = upsample_bilinear(up_layer_3, scale_factor=2)
+        up_layer_2 = interpolate(up_layer_3, scale_factor=2, mode='bilinear')
         up_layer_2 = ConvTranspose2d(256, 128, 2)(up_layer_2)
         up_layer_2 = torch.cat((layer2_crop, up_layer_2), dim=0)
         up_layer_2 = self.uplayer_2_conv1(up_layer_2)
@@ -103,7 +103,7 @@ class UNET(Module):
         up_layer_2 = self.uplayer_2_conv2(up_layer_2)
         up_layer_2 = relu(up_layer_2)
 
-        up_layer_1 = upsample_bilinear(up_layer_2, scale_factor=2)
+        up_layer_1 = interpolate(up_layer_2, scale_factor=2, mode='bilinear')
         up_layer_1 = ConvTranspose2d(128, 64, 2)(up_layer_1)
         up_layer_1 = torch.cat((layer1_crop, up_layer_1), dim=0)
         up_layer_1 = self.uplayer_1_conv1(up_layer_1)
