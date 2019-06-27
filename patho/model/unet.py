506a701dc3b01ce6025902b7ed3984c032b75dae
Alex Olar <olaralex666@gmail.com>
2019-06-27 16:00:27 +0200

    dimensional issue... long way of debugging aheAD

--- patho/model/unet.py
+++ patho/model/unet.py
@@ -70,30 +70,33 @@ class UNET(Module):
         layer3_crop = pad(layer3, (-12, -12))
         layer4_crop = pad(layer4, (-4, -4))
 
+        print(layer4.shape, layer4_crop.shape)
+
         # Up forward pass
         up_layer_4 = ConvTranspose2d(1024, 512, 2)(layer5)
-        up_layer_4 = torch.cat((layer4, up_layer_4), dim=0)
+        print(up_layer_4.shape)
+        up_layer_4 = torch.cat((layer4_crop, up_layer_4), dim=0)
         up_layer_4 = self.uplayer_4_conv1(up_layer_4)
         up_layer_4 = relu(up_layer_4)
         up_layer_4 = self.uplayer_4_conv2(up_layer_4)
         up_layer_4 = relu(up_layer_4)
 
         up_layer_3 = ConvTranspose2d(512, 256, 2)(up_layer_4)
-        up_layer_3 = torch.cat((layer3, up_layer_3), dim=0)
+        up_layer_3 = torch.cat((layer3_crop, up_layer_3), dim=0)
         up_layer_3 = self.uplayer_3_conv1(up_layer_3)
         up_layer_3 = relu(up_layer_3)
         up_layer_3 = self.uplayer_3_conv2(up_layer_3)
         up_layer_3 = relu(up_layer_3)
 
         up_layer_2 = ConvTranspose2d(256, 128, 2)(up_layer_3)
-        up_layer_2 = torch.cat((layer2, up_layer_2), dim=0)
+        up_layer_2 = torch.cat((layer2_crop, up_layer_2), dim=0)
         up_layer_2 = self.uplayer_2_conv1(up_layer_2)
         up_layer_2 = relu(up_layer_2)
         up_layer_2 = self.uplayer_2_conv2(up_layer_2)
         up_layer_2 = relu(up_layer_2)
 
         up_layer_1 = ConvTranspose2d(128, 64, 2)(up_layer_2)
-        up_layer_1 = torch.cat((layer1, up_layer_1), dim=0)
+        up_layer_1 = torch.cat((layer1_crop, up_layer_1), dim=0)
         up_layer_1 = self.uplayer_1_conv1(up_layer_1)
         up_layer_1 = relu(up_layer_1)
         up_layer_1 = self.uplayer_1_conv2(up_layer_1)
