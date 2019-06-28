9df4f36352b95e6e11f2fadd410abe0223e821ed
Alex Olar <olaralex666@gmail.com>
2019-06-28 15:06:01 +0200

    added reference to custom loss

--- patho/custom_loss/loss.py
+++ patho/custom_loss/loss.py
@@ -2,6 +2,10 @@ from torch.nn import Module
 from torch.nn import BCELoss
 import torch
 
+################################################
+##### https://arxiv.org/pdf/1801.05746.pdf #####
+################################################
+
 
 class BCELossWithJaccard(Module):
     def __init__(self):
@@ -10,8 +14,6 @@ class BCELossWithJaccard(Module):
     def forward(self, x, y):
         bce = BCELoss()
         loss1 = bce(x, y)
-        x_ = x.view(-1)
-        y_ = y.view(-1)
         xy_ = x_*y_
         loss2 = torch.mean(xy_ / (x_ + y_ - xy_))
         return loss1 - torch.log(loss2 + 1e-12)
