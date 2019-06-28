63bae1e10ed6969cf9709d78545c295ede8fe1dc
Alex Olar <olaralex666@gmail.com>
2019-06-28 15:35:06 +0200

    print average loss on batch end

--- patho/model/model.py
+++ patho/model/model.py
@@ -15,6 +15,7 @@ class Model:
     def train(self, data_loader, EPOCH=100):
         for epoch in range(EPOCH):
 
+            loss_on_epoch_end = 0.0
             running_loss = 0.0
             for ind, (image, segmentation_map) in enumerate(data_loader, 0):
                 image, segmentation_map = image.to(
@@ -29,9 +30,12 @@ class Model:
                 self.optimizer.step()
 
                 running_loss += loss.item()
+                loss_on_epoch_end += loss.item()
                 if ind % 10 == 9:    # print every 10 mini-batches
                     print('[%d, %5d] loss: %.3f' %
                           (epoch + 1, ind + 1, running_loss / 10))
                     running_loss = 0.0
+            loss_on_epoch_end /= (ind + 1) # batch average loss
+            print("Average batch loss on epoch end : %.5f" % loss_on_epoch_end)
 
         print('Finished training!')
