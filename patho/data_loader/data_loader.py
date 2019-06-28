e308d5da16cc0dd747383b1fca643c2a06eb2e3f
Alex Olar <olaralex666@gmail.com>
2019-06-28 15:52:09 +0200

    transposed images instead of reshaping them

--- patho/data_loader/data_loader.py
+++ patho/data_loader/data_loader.py
@@ -4,13 +4,13 @@ import torch.utils.data
 
 
 class DataLoader:
-    def __init__(self, root_data_dir, images_dir, masks_dir):
+    def __init__(self, root_data_dir, images_dir, masks_dir, batch_size=2):
         dataset = DataSet(root_data_dir, images_dir, masks_dir)
         indices = torch.randperm(len(dataset)).tolist()
         dataset = torch.utils.data.Subset(dataset, indices)
 
         self.INSTANCE = torch.utils.data.DataLoader(
-            dataset, batch_size=3, shuffle=True, num_workers=4)
+            dataset, batch_size=batch_size, shuffle=True, num_workers=1)
 
     def getInstance(self):
         return self.INSTANCE
