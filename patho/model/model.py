9323f13de8907f97baaa7d4f1a33b80c9a33979b
Alex Olar <olaralex666@gmail.com>
2019-06-29 07:13:39 +0200

    typos

--- patho/model/model.py
+++ patho/model/model.py
@@ -10,10 +10,10 @@ class Model:
         self.device = torch.device(
             'cuda') if torch.cuda.is_available() else torch.device('cpu')
         self.net = net
+        self.net.to(self.device)
         if load_model:
             self.net.load_state_dict(
                 torch.load("patho/data/model.pt"))
-        self.net.to(self.device)
         self.criterion = BCELoss()
         if with_jaccard:
             self.criterion = BCELossWithJaccard()
