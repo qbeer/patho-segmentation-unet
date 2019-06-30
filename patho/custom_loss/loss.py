02bc505c389c38cb69a11a9cdecdd3ceb6865d63
Alex Olar <olaralex666@gmail.com>
2019-06-30 11:40:03 +0200

    one-hot encode jaccard

--- patho/custom_loss/loss.py
+++ patho/custom_loss/loss.py
@@ -11,8 +11,13 @@ class BCELossWithJaccard(Module):
     def __init__(self):
         super(BCELossWithJaccard, self).__init__()
 
-    def forward(self, x, y):
+    def forward(self, x, y, eps=1e-12):
         bce = BCELoss()(x, y)
+        # One-hot encode
+        x[x >= 0.5] = 1.
+        x[x < 0.5] = 0.
+        y[y >= 0.5] = 1.
+        y[y < 0.5] = 0.
         xy = x*y
         jaccard = torch.mean(xy / (x + y - xy))
-        return bce - torch.log(jaccard + 1e-12)
+        return bce - torch.log(jaccard + eps)
