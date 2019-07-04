2978256e76e7173689a259c0bdade018f3bec241
Alex Olar <olaralex666@gmail.com>
2019-07-04 10:44:00 +0200

    added back with_jaccard option

--- patho/custom_loss/loss.py
+++ patho/custom_loss/loss.py
@@ -10,9 +10,10 @@ import torch
 class BCELossWithJaccard(Module):
     def __init__(self):
         super(BCELossWithJaccard, self).__init__()
+        self.bce = BCELoss()
 
     def forward(self, x, y):
-        bce = BCELoss()(x, y)
+        bce = self.bce(x, y)
         xy = x*y
         jaccard = torch.mean(xy / (x + y - xy))
         return bce - torch.log(jaccard + 1e-12)
