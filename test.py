2d0a2721f2a6c33448bc99c9098c38a706052d2d
Alex Olar <qbeer666@q.krft>
2019-08-05 17:17:17 +0200

    test update

--- test.py
+++ test.py
@@ -1,14 +1,52 @@
 from patho import Model, UNET_VGG, DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
 
 data_loader = DataLoader("patho/data/crc",
                          "images",
                          "masks",
                          batch_size=1,
                          output_size=572).getInstance()
+
 unet = UNET_VGG()
+unet.make_parallel()
 
-model = Model(unet)
+model = Model(unet, lr=5e-6, with_jaccard=True, load_model=True)
 
-print("Init done.")
+for imgs, masks in data_loader:
+    predicted_masks = model.net(imgs.cuda()).cpu().detach().numpy()
+    imgs, masks = imgs.cpu().detach().numpy(), masks.cpu().detach().numpy()
+    break
 
-model.train(data_loader, EPOCH=1)
+fig, axes = plt.subplots(3, 3, figsize=(9, 9), sharex=True, sharey=True)
+
+for ind, ax in enumerate(axes.flatten(), 0):
+    if ind % 3 == 0:
+        ax.imshow(imgs[ind // 3].transpose((1, 2, 0)))
+    elif ind % 3 == 1:
+        ax.imshow(masks[ind // 3].transpose((1, 2, 0)).reshape(572, 572))
+    else:
+        ax.imshow(predicted_masks[ind // 3].transpose(
+            (1, 2, 0)).reshape(572, 572))
+
+fig.tight_layout()
+plt.savefig("raw.png")
+plt.show()
+
+predicted_masks[predicted_masks >= 0.75] = 1.
+predicted_masks[predicted_masks < 0.75] = 0.
+
+fig, axes = plt.subplots(3, 3, figsize=(9, 9), sharex=True, sharey=True)
+
+for ind, ax in enumerate(axes.flatten(), 0):
+    if ind % 3 == 0:
+        ax.imshow(imgs[ind // 3].transpose((1, 2, 0)))
+    elif ind % 3 == 1:
+        ax.imshow(masks[ind // 3].transpose((1, 2, 0)).reshape(572, 572))
+    else:
+        ax.imshow(predicted_masks[ind // 3].transpose(
+            (1, 2, 0)).reshape(572, 572))
+
+fig.tight_layout()
+plt.savefig("075cut.png")
+plt.show()
